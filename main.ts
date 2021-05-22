1eb8974a5a782ee9a07b462b2c223db3eb68a4fa
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2021-05-22 16:59:10 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
 pins.servoWritePin(AnalogPin.P0, 0)
 basic.forever(function () {
     if (input.lightLevel() == 0) {
         basic.showIcon(IconNames.Happy)
         pins.servoWritePin(AnalogPin.P0, 90)
-        basic.pause(1000)
+        basic.pause(200)
         pins.servoWritePin(AnalogPin.P0, 0)
-        basic.pause(2000)
+        basic.pause(200)
+        pins.servoWritePin(AnalogPin.P0, 90)
+        basic.pause(200)
+        pins.servoWritePin(AnalogPin.P0, 0)
     } else {
         basic.showIcon(IconNames.Asleep)
         pins.servoWritePin(AnalogPin.P0, 0)
